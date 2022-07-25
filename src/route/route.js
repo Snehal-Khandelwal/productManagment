216a95d10f265c539e8263a56ed311fbacf38dc4
ankit6823 <106534621+ankit6823@users.noreply.github.com>
2022-07-25 15:20:38 +0530

    Update route.js

--- src/route/route.js
+++ src/route/route.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const userControllers = require('../controllers/userControllers.js')
 
 router.post('/register', userControllers.createUser)
+router.post('/login',userControllers.userLogin )
+
 
 
 module.exports = router
